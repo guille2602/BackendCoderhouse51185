f3eaa9750bd01a792087f510c02e1e585aa2c060
Guillermo Seclen Alfaro <86248307+guille2602@users.noreply.github.com>
2023-05-16 17:10:52 -0300

    Preentrega completada

--- 5. Practica ingradora/src/routes/views.routes.js
+++ 5. Practica ingradora/src/routes/views.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productsModel from "../dao/models/products.model.js";
-import MongoproductManager from "../dao/mongoManagers/dbProductManager.js";
+import MongoProductManager from "../dao/mongoManagers/dbProductManager.js";
+import MongoCartManager from "../dao/mongoManagers/dbCartManager.js";
 
 const router = Router();
-const productManager = new MongoproductManager();
+const productManager = new MongoProductManager();
+const cartManager = new MongoCartManager();
 
 router.get("/realtimeproducts", async (req, res) => {
     try {
@@ -25,7 +27,7 @@ router.get("/realtimeproducts", async (req, res) => {
     } catch (error) {
         console.log("Error al leer la base de datos de MongoDB", +error);
     }
-})
+});
 
 router.get("/", async (req, res) => {
     const { limit = 10, sort = null, page = 1, query = null } = req.query;
@@ -40,21 +42,8 @@ router.get("/", async (req, res) => {
         hasNextPage,
         prevLink,
         nextLink,
-    } = await productManager.paginateContent( limit, sort, page, query );
-    // res.render("home", {
-    //     css: "home.css", 
-    //     code,
-    //     status,
-    //     payload,
-    //     totalPages,
-    //     prevPage,
-    //     nextPage,
-    //     page,
-    //     hasPrevPage,
-    //     hasNextPage,
-    //     prevLink,
-    //     nextLink,
-    // })
+    } = await productManager.paginateContent(limit, sort, page, query);
+
     res.status(code).send({
         status,
         payload,
@@ -66,12 +55,10 @@ router.get("/", async (req, res) => {
         hasNextPage,
         prevLink,
         nextLink,
-    })
-
-})
+    });
+});
 
 router.get("/products", async (req, res) => {
-
     const { limit = 10, sort = null, page = 1, query = null } = req.query;
     const {
         code,
@@ -84,10 +71,10 @@ router.get("/products", async (req, res) => {
         hasNextPage,
         prevLink,
         nextLink,
-    } = await productManager.paginateContent( limit, sort, page, query );
-    
+    } = await productManager.paginateContent(limit, sort, page, query);
+
     res.render("products", {
-        css: "../css/products.css", 
+        css: "../css/products.css",
         code,
         status,
         payload,
@@ -99,15 +86,25 @@ router.get("/products", async (req, res) => {
         hasNextPage,
         prevLink,
         nextLink,
-    })
-
-    //3. Agregar botones para agregar a carrito > agregar Swal > ingresar ID de carrito
-
-})
+    });
+});
 
 router.get("/carts/:cid", async (req, res) => {
-    //1. Recibir el contenido del carrito
-    //2. Renderizar la vista
-})
+    const { payload } = await cartManager.readCart( req.params.cid );
+    let parsedList = payload.products.map((item) => ({
+        id: item.product.id,
+        title: item.product.title,
+        description: item.product.description,
+        code: item.product.code,
+        price: item.product.price,
+        category: item.product.category,
+        quantity: item.quantity,
+    }));
+    console.log(parsedList)
+    res.render("cart", {
+        css: "../../css/home.css",
+        payload: parsedList
+    });
+});
 
 export default router;
