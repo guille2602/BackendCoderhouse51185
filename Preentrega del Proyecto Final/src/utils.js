3d750c6bbd5fa713921eb48354d4469c324a86e9
Guillermo Seclen Alfaro <86248307+guille2602@users.noreply.github.com>
2023-07-10 00:27:42 -0300

    Checkpoint

--- Preentrega del Proyecto Final/src/utils.js
+++ Preentrega del Proyecto Final/src/utils.js
@@ -1,12 +1,49 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
+import { v4 } from "uuid";
 
-//Configuraciones de path
+//Absolut path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export default __dirname;
 
-//Configuraciones de bcrypt
+//bcrypt
 export const createHash = ( password ) => bcrypt.hashSync( password , bcrypt.genSaltSync(10) );
 export const validatePassword = ( user , password ) => bcrypt.compareSync( password , user.password );
+
+//Faker
+faker.location = 'es';
+
+export const generateProduct = () => {
+    const category = faker.commerce.department();
+    const code = concat(category.slice(0,3), v4())
+    return {
+        title: faker.commerce.product(),
+        description: faker.commerce.productDescription(),
+        code,
+        price: faker.commerce.price(),
+        stock: faker.number.int({ min: 1, max: 200}),
+        category,
+        thumbnail: [faker.image.url()]
+    }
+}
+
+export const generateUsers = () => {
+    let productsList = [];
+    const cantProducts = Math.floor(Math.random()*10)
+    for ( i = 1; i <= cantProducts; i++){
+        productsList.push(generateProduct())
+    }
+    return {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: "",
+        age: faker.number.int({ min: 18, max: 99}),
+        password: "",
+        cart: productsList,
+        role: faker.datatype.boolean ? 'user' : 'admin'
+    }
+
+}
